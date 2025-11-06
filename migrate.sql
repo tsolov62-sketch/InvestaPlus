2360ba2b4bba3354ea4bec309efe4695d74cee3d
tsolov62-sketch <tsolov62@gmail.com>
2025-11-06 22:37:34 +0200

    Delete migrate.sql

--- migrate.sql
+++
@@ -1,4 +0,0 @@
--- For Postgres migration, example:
-CREATE TABLE users (id SERIAL PRIMARY KEY, email TEXT UNIQUE, password TEXT, created_at TIMESTAMP DEFAULT NOW());
-CREATE TABLE balances (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users(id), balance NUMERIC DEFAULT 0, last_update TIMESTAMP DEFAULT NOW());
-CREATE TABLE history (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users(id), type TEXT, amount NUMERIC, date DATE);
